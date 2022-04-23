15083f0670962cbaaac614eeaf9b55ec51fdb1e8
Graham <graham.moss@hotmail.com>
2022-04-23 15:55:58 -0400

    finish distance calculations

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -5,11 +5,13 @@ function Calculator() {
   const [calcType, setCalcType] = useState('pace')
   const [miles, setMiles] = useState(false)
   const [time, setTime] = useState({ hours: 0, minutes: 0, seconds: 0 })
+  const [pace, setPace] = useState({ minutes: 0, seconds: 0 })
   const [distance, setDistance] = useState(0)
   const [answer, setAnswer] = useState('')
 
   const unitChange = (e: ChangeEvent<HTMLInputElement>) => {
     setMiles(e.target.checked)
+    clearInputs()
   }
 
   const calcChange = (e: ChangeEvent<HTMLSelectElement>) => {
@@ -19,6 +21,10 @@ function Calculator() {
 
   const timeInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setTime((prev) => ({ ...prev, [e.target.id]: Number(e.target.value) }))
+  }
+
+  const paceInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPace((prev) => ({ ...prev, [e.target.id]: Number(e.target.value) }))
   }
 
   const distInputChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -37,26 +43,33 @@ function Calculator() {
     }
   }
 
-
   const calculate = () => {
 
     let t: number = 0
 
+    let totalTime = (time.hours * 3600) + (time.minutes * 60) + time.seconds
+
     switch (calcType) {
-      case 'pace': t = ((time.hours * 3600) + (time.minutes * 60) + time.seconds) / distance; break;
-      case 'time': t = (time.minutes * 60 + time.seconds) * distance; break;
+      case 'pace': t = totalTime / distance; break;
+      case 'time': t = (pace.minutes * 60 + pace.seconds) * distance; break;
+      case 'distance': t = totalTime / (pace.minutes * 60) + pace.seconds; break;
     }
 
     let h: number = Math.floor(t / 3600)
     let m: number = Math.floor(t % 3600 / 60)
     let s: number = Math.floor(t % 3600 % 60)
-    console.log((time.minutes * 60 + time.seconds) * distance);
 
-    setAnswer(h === 0 ? formatTime(m) + ':' + formatTime(s) : formatTime(h, true) + ':' + formatTime(m) + ':' + formatTime(s))
+    if (calcType === 'distance') {
+      setAnswer(t.toString() + (miles ? ' Miles' : ' Kilometers'))
+    } else {
+      setAnswer(h === 0 ? formatTime(m) + ':' + formatTime(s) : formatTime(h, true) + ':' + formatTime(m) + ':' + formatTime(s))
+    }
+
   }
 
   const clearInputs = () => {
     setTime({ hours: 0, minutes: 0, seconds: 0 })
+    setPace({ minutes: 0, seconds: 0 })
     setDistance(0)
     setAnswer('')
   }
@@ -101,11 +114,11 @@ function Calculator() {
             <p className="inputs-title">Pace</p>
             <div className="input-row">
               <div>
-                <input maxLength={2} placeholder="min" value={time.minutes} id="minutes" onChange={timeInputChange} />
+                <input maxLength={2} placeholder="min" value={pace.minutes} id="minutes" onChange={paceInputChange} />
                 <p>Mins</p>
               </div>
               <div>
-                <input maxLength={2} placeholder="sec" value={time.seconds} id="seconds" onChange={timeInputChange} />
+                <input maxLength={2} placeholder="sec" value={pace.seconds} id="seconds" onChange={paceInputChange} />
                 <p>Secs</p>
               </div>
               <p>{miles ? 'per mile' : 'per kilometer'}</p>
@@ -132,13 +145,16 @@ function Calculator() {
         <button onClick={clearInputs}>Clear</button>
       </div>
 
-      {answer !== '' &&
+      {(answer !== '' && !answer.includes('NaN')) &&
         <div className="answer-container">
           {
             calcType === 'pace' && <p className="inputs-title">Pace per {miles ? 'mile' : 'kilometer'}</p>
           }
           {
             calcType === 'time' && <p className="inputs-title">Time</p>
+          }
+          {
+            calcType === 'distance' && <p className="inputs-title">Distance</p>
           }
 
           <p className="answer">{answer}</p>
