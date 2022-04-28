299305c765bba10c56a9f3eddf456bd4156dc1ee
Graham <graham.moss@hotmail.com>
2022-04-28 19:05:51 -0400

    get and create entries

--- backend/controllers/entryController.js
+++ backend/controllers/entryController.js
@@ -4,11 +4,40 @@ const User = require('../models/userModel')
 const Entry = require('../models/entryModel')
 
 const getEntries = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'getEntries' })
+  // Get user using the ID and JWT
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    // Unauthorized
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const entries = await Entry.find({ user: req.user.id })
+  res.status(200).json(entries)
 })
 
 const createEntry = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'createEntry' })
+  const { date, speed } = req.body
+
+  if (!date || !speed) {
+    // Bad Request
+    res.status(400)
+    throw new Error('Please add date and speed')
+  }
+
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    // Unauthorized
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const entry = await Entry.create({ date, speed, user: req.user.id })
+
+  // Created
+  res.status(201).json(entry)
 })
 
 module.exports = { getEntries, createEntry }
