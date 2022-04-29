dd02d4174ef86eb512e8ad00bf183e559a97ebcc
Graham <graham.moss@hotmail.com>
2022-04-28 21:14:38 -0400

    update & delete entries on backend

--- backend/controllers/entryController.js
+++ backend/controllers/entryController.js
@@ -17,6 +17,33 @@ const getEntries = asyncHandler(async (req, res) => {
   res.status(200).json(entries)
 })
 
+
+const getEntry = asyncHandler(async (req, res) => {
+  // Get user using the ID and JWT
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    // Unauthorized
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const entry = await Entry.findById(req.params.id)
+
+  if (!entry) {
+    res.status(404)
+    throw new Error('Entry not found')
+  }
+
+  if (entry.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not Authorized')
+  }
+
+  res.status(200).json(entry)
+})
+
+
 const createEntry = asyncHandler(async (req, res) => {
   const { date, speed } = req.body
 
@@ -40,4 +67,59 @@ const createEntry = asyncHandler(async (req, res) => {
   res.status(201).json(entry)
 })
 
-module.exports = { getEntries, createEntry }
+
+const deleteEntry = asyncHandler(async (req, res) => {
+  // Get user using the ID and JWT
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    // Unauthorized
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const entry = await Entry.findById(req.params.id)
+
+  if (!entry) {
+    res.status(404)
+    throw new Error('Entry not found')
+  }
+
+  if (entry.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not Authorized')
+  }
+
+  await entry.remove()
+
+  res.status(200).json({ success: true })
+})
+
+const updateEntry = asyncHandler(async (req, res) => {
+  // Get user using the ID and JWT
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    // Unauthorized
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const entry = await Entry.findById(req.params.id)
+
+  if (!entry) {
+    res.status(404)
+    throw new Error('Entry not found')
+  }
+
+  if (entry.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not Authorized')
+  }
+
+  const updatedEntry = await Entry.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+  res.status(200).json(updatedEntry)
+})
+
+module.exports = { getEntries, getEntry, createEntry, deleteEntry, updateEntry }
--- backend/routes/entryRoutes.js
+++ backend/routes/entryRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { protect } = require('../middleware/authMiddleware')
-const { getEntries, createEntry } = require('../controllers/entryController')
+const { getEntries, getEntry, createEntry, deleteEntry, updateEntry } = require('../controllers/entryController')
 
 router.route('/').get(protect, getEntries).post(protect, createEntry)
+router.route('/:id').get(protect, getEntry).delete(protect, deleteEntry).put(protect, updateEntry)
 
 module.exports = router
